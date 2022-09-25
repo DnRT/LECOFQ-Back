8d69027b0e69ede02df3467d592bc8ec62dfa333
DnRT <kn_ct_5290@hotmail.com>
2022-09-25 16:24:50 -0300

    cambio query

--- back/src/controllers/posts.controller.js
+++ back/src/controllers/posts.controller.js
@@ -1,10 +1,11 @@
 import {getConn} from '../database/database';
 
 const getPosts = async (req,res) =>{
+    const query='SELECT ID,post_author,post_date,post_date_gmt,post_content,post_title,post_status,post_type FROM ds_posts';
     try {
         const conn = await getConn();
-        const result = await conn.query('SELECT * FROM ds_posts');
-        res.json(result);
+        const result = await conn.query(query);
+        return res.json(result);
     } catch (error) {
         res.send(error.message);
     }
