d245314631e2db530f085b53b5713ddf338923a5
DnRT <kn_ct_5290@hotmail.com>
2022-09-28 13:48:58 -0300

    creado de json

--- back/src/controllers/posts.controller.js
+++ back/src/controllers/posts.controller.js
@@ -1,10 +1,24 @@
 import {getConn} from '../database/database';
 
+const fs=require('fs');
+
+function createJSON(name,content){
+    var arch=fs.writeFile(name+".json", content,(err)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            console.log("JSON Created");
+        }
+    });
+}
+
 const getPosts = async (req,res) =>{
     const query='SELECT ID,post_author,post_date,post_date_gmt,post_content,post_title,post_status,post_type FROM ds_posts';
     try {
         const conn = await getConn();
         const result = await conn.query(query);
+        createJSON('all_news',JSON.stringify(result));
         return res.json(result);
     } catch (error) {
         res.send(error.message);
@@ -17,6 +31,7 @@ const getPost= async (req,res)=>{
     try {
         const conn = await getConn();
         const result = await conn.query(query,id);
+        createJSON('new_id_'+id,JSON.stringify(result));
         return res.json(result);
     } catch (error) {
         res.send(error.message);
